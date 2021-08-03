a2f47f7a38c3773effd40cc97b4c5d34e71d5fb6
LEESEOYU <roseelsy@naver.com>
2021-08-03 21:23:45 +0900

    api 연결

---
+++ Project_01/src/RollS1.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const name= () =>{
+    
+}
+
+const RollS1 = () => {
+    return (
+        <div className="g1">
+            <h2 className="grade1">1학년</h2>
+            <table>
+                <tr><td>이름</td><td>출석</td></tr>
+                <tr><td>이서유</td><td></td></tr>
+                <tr><td>이서유</td><td></td></tr>
+                <tr><td>이서유</td><td></td></tr>
+                <tr><td>이서유</td><td></td></tr>
+                <tr><td>이서유</td><td></td></tr>
+                <tr><td>이서유</td><td></td></tr>
+            </table>
+        </div>
+    )
+}
+
+export default RollS1;
+
