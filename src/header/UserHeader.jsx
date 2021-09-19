060658ec7663c16831a21e2105fe29933a9697f7
baejoonsoo <baejoonsoo21@gmail.com>
2021-09-20 01:05:16 +0900

    user header

--- src/header/UserHeader.jsx
+++ src/header/UserHeader.jsx
@@ -1,32 +1,33 @@
-import { Link } from "react-router-dom";
-import "./Header.css";
-import logo from "../img/infoIcon.png";
+import { Link } from 'react-router-dom';
+import './Header.css';
+import logo from '../img/infoIcon.png';
+import * as S from './style';
 
 const UserHeader = () => {
-    return (
-        <div className="header">
-            <Link className="printToLogo" to="/user">
-                <img src={logo} alt="infoIcon" />
-            </Link>
-            <div className="tool">
-                <span className="lAndS_Button">
-                    <Link to="/user/login" className="loginAndSignup">
-                        Login
-                    </Link>
-                    &nbsp;|&nbsp;
-                    <Link to="/user/signup" className="loginAndSignup">
-                        Signup
-                    </Link>
-                </span>
-                <Link className="button" to="/user/rollCall">
-                    출석 체크
-                </Link>
-                <Link className="button" to="/user/showList">
-                    일정 목록
-                </Link>
-            </div>
-        </div>
-    );
+  return (
+    <S.Header>
+      <Link to="/user">
+        <S.MainLogo src={logo} alt="mainIcon" />
+      </Link>
+      <S.Tool>
+        <S.LoginAndSignup>
+          <Link to="/user/login">
+            <S.Button>Login</S.Button>
+          </Link>
+          &nbsp;|&nbsp;
+          <Link to="/user/signup">
+            <S.Button>Signup</S.Button>
+          </Link>
+        </S.LoginAndSignup>
+        <Link to="/user/rollCall">
+          <S.NavButton>출석 체크</S.NavButton>
+        </Link>
+        <Link to="/user/showList">
+          <S.NavButton>일정 목록</S.NavButton>
+        </Link>
+      </S.Tool>
+    </S.Header>
+  );
 };
 
 export default UserHeader;
---
+++ src/header/style.js
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  background-color: #232323;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5vh;
+  height: 6vh;
+`; //높이 7vh
+
+export const MainLogo = styled.img`
+  padding-left: 10px;
+  height: 90%;
+`;
+
+export const Tool = styled.nav`
+  display: flex;
+  align-items: center;
+`;
+
+export const LoginAndSignup = styled.span`
+  margin-right: 20px;
+  color: white;
+  cursor: pointer;
+  text-decoration: none;
+  width: 100px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+  font-size: 100%;
+`;
+
+export const Button = styled.p`
+  font-weight: bold;
+  font-size: 15px;
+  color: white;
+  cursor: pointer;
+  text-decoration: none;
+
+  &:hover {
+    font-size: 1.1em;
+    color: greenyellow;
+  }
+`;
+
+export const NavButton = styled.p`
+  font-weight: bold;
+  font-size: 15px;
+  color: white;
+  cursor: pointer;
+  text-decoration: none;
+  margin-right: 20px;
+
+  &:hover {
+    font-size: 1.1em;
+    color: greenyellow;
+  }
+`;
